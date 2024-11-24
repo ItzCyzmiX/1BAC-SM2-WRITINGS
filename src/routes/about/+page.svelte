c76535608b353ef6a95dc50bf3a7565bf1c093b4
CyzmiX <98268356+ItzCyzmiX@users.noreply.github.com>
2024-11-24 20:54:53 +0000

    first commit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About this project</h1>
+
+	<p>
+		This is a class project, that revolves around creative minds of the 1BAC SM2 class. In this website you will find wrintings of the briliant students of this class, were they express theire varying opinions on differents topics!
+	</p>
+
+	<pre>FUN READIN!</pre>
+
+	<p>
+		The <a href="/galerie">Galerie</a> page contains all the writings!
+	</p>
+
+	<h1>Current contributers</h1>
+	none
+</div>
---
+++ src/routes/galerie/+page.svelte
@@ -0,0 +1,8 @@
+
+<svelte:head>
+	<title>Galerie</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+
+no writings are here yet!
